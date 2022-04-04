ad3caeedc2d6ec111f677e5a2af900f06dc815e2
wcktklwn <wcktklwn@wcktklwn.net>
2022-04-04 16:44:27 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,6 +1,9 @@
-with open('Lists/RAW/allowlist.txt') as f:
+with open('Lists/RAW/allowlist.txt', 'r') as f:
     allowlist = f.read().splitlines() 
 
+adguardallow = open('Lists/AdGuard/allowlist.txt', 'w')
+allowlines = []
 
-for allow in allowlist():
-    print("@@||%s^" % (allow))
+for allow in allowlist:
+    allowlines.append("@@||%s^" % (allow))
+    print(allowlines)
